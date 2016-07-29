26314701d945a0579c6c4c95a7acbaa6f446ef1b
andrea <andrea@gmail.com>
2016-07-29 12:55:25 +0200

    File temporanei e help aggiunti.

--- dispatch/dispatch.cpp
+++ dispatch/dispatch.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
-int main() {
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	FILE *patch,*data, *result;
+	patch= fopen(argv[1], "r");
+	data = fopen(argv[2], "r");
+	result=fopen(argv[3], "w");
+
+	char * line = NULL;
+    	size_t len = 0;
+    	ssize_t read;
+	while ( (read = getline(&line, &len, patch)) != -1) {
+        	
+    	}
 
 	return 1;
 }
